25ef02db14aa45fb8cc21e6d0b13fad3874f626f
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-11-15 12:23:16 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+// Package metadata for Meteor.js
+
+Package.describe({
+  name: "todos",
+  summary: "A todo app for the iSC Inc. team built in Meteor.",
+  version: "1.0.0",
+  git: "https://github.com/iSCInc/todo.git"
+});
+
+Package.onUse(function(api) {
+});
